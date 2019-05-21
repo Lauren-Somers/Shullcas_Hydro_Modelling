79acc4eaba0c91bf384ba2267bbf8851ca0f3cce
Lauren Somers <Lauren.somers@mail.mcgill.ca>
2019-05-21 11:58:17 -0400

    Add files via upload

---
+++ Shullcas_12_1/control/Shullcas_12_1_GSFLOW.bat
@@ -0,0 +1,9 @@
+SET CURRENTDIR="%cd%" 
+cd D:\GSFLOW\GSFLOW_Runs\Shullcas_12_GSFLOW-GRASS\Shullcas_12_1\control
+
+ECHO Running GSFLOW in D:\GSFLOW\GSFLOW_Runs\Shullcas_12_GSFLOW-GRASS\Shullcas_12_1: 
+ECHO 'D:\GSFLOW\GSFLOW_2.0.0_beta_exe\gsflow_beta.exe Shullcas_12_1_GSFLOW.control' 
+
+D:\GSFLOW\GSFLOW_2.0.0_beta_exe\gsflow_beta.exe Shullcas_12_1_GSFLOW.control
+
+cd %CURRENTDIR% 
